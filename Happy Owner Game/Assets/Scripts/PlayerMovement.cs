672c0fea0676e8a2b52fa6c404f309ed62fa9ff9
Dion Vos <dionvos160@gmail.com>
2023-05-11 14:06:08 +0200

    Added player movement and camera

---
+++ Happy Owner Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _speed;
+    private Vector2 _moveDirection;
+    private bool _grounded;
+    private float _jumpForce;
+
+    private Rigidbody2D _rb;
+    public BoxCollider2D groundTrigger;
+
+    public InputAction leftRight;
+
+    public float Speed
+    {
+        get => _speed;
+        set => _speed = value;
+    }
+    private void Awake()
+    {
+        _jumpForce = 400;
+        _speed = 10;
+        
+        _rb = GetComponent<Rigidbody2D>();
+        groundTrigger = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        _moveDirection = leftRight.ReadValue<Vector2>();
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            Jump();
+    }
+
+    private void FixedUpdate() => Movement();
+
+    private void Movement() => _rb.velocity = new Vector2(_moveDirection.x * _speed, _rb.velocity.y);
+
+    private void Jump()
+    {
+        if (_grounded) 
+            _rb.AddForce(new Vector2(0, _jumpForce));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) => _grounded = true;
+
+    private void OnEnable() => leftRight.Enable();
+
+    private void OnDisable() => leftRight.Disable();
+}
